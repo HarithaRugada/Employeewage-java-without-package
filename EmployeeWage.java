1b39eb4f5c0ee3f25f6c4c4a663e8eda15145f96
HarithaRugada <haritharugada@gmail.com>
2020-05-11 19:21:06 +0530

    added usecase5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,29 +5,38 @@ public class EmployeeWage
         {
 
                 System.out.println("Welcome to Employee Wage Computation Program");
-		double empCheck=Math.random();
-
-                if (empCheck >0.5)
+		double salary=0;
+		for(int i=1;i<=20;i++)
 		{
-                        System.out.println("Employee is Present");
-			int wagePerHour=20;
-			double workHours=Math.floor(Math.random()*10)%10;
-			switch ((int) workHours)
+
+			double empCheck=Math.random();
+	                if (empCheck >0.5)
 			{
-				case 9:
-					System.out.println("Full time");
-					break;
-				default:
-					System.out.println("Part time");
+        	                System.out.println("Employee is Present on Day " +i);
+				int wagePerHour=20;
+				double workHours=Math.floor(Math.random()*10)%10;
+
+				switch ((int) workHours)
+				{
+					case 9:
+						System.out.println("Full time");
+						break;
+					default:
+						System.out.println("Part time");
+				}
+
+				double dailyWage=wagePerHour*(workHours+1);
+				salary += dailyWage;
+				System.out.println("Hours present on Day " +i+ " is " +(workHours+1));
+				System.out.println("The daily wage of employee on Day " +i+ " is " +dailyWage);
+				System.out.println("Salary till Day " +i+ " is " +salary);
 			}
-			double dailyWage=wagePerHour*(workHours+1);
-			System.out.println("Hours present is "+(workHours+1));
-			System.out.println("The daily wage of employee is " +dailyWage);
+
+        	        else
+                 	       System.out.println("Employee is Absent on Day " +i);
+
 		}
 
-                else
-                        System.out.println("Employee is Absent");
-
-        }
+	}
 
 }
