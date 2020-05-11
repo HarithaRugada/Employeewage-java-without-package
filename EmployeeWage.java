ccabaddeea477373cc77ecde42ce2e7be1f1d5c9
HarithaRugada <haritharugada@gmail.com>
2020-05-11 17:24:14 +0530

    added usecase4

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,8 +10,16 @@ public class EmployeeWage
                 if (empCheck >0.5)
 		{
                         System.out.println("Employee is Present");
-			double workHours=Math.floor(Math.random()*10)%10;
 			int wagePerHour=20;
+			double workHours=Math.floor(Math.random()*10)%10;
+			switch ((int) workHours)
+			{
+				case 9:
+					System.out.println("Full time");
+					break;
+				default:
+					System.out.println("Part time");
+			}
 			double dailyWage=wagePerHour*(workHours+1);
 			System.out.println("Hours present is "+(workHours+1));
 			System.out.println("The daily wage of employee is " +dailyWage);
